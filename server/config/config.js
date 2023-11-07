3cf678b68e61b5f9493b18d16cab2c162569e2f4
iamhashaan <tkhashaanadeen@gmail,com>
2023-11-07 18:43:00 +0530

    fina

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    dbURI: 'mongodb+srv://satvik:satvik@agrobuy.ftzut.mongodb.net/?retryWrites=true&w=majority',
+    dbURI: 'mongodb+srv://tkhashaamadeen527:$Hs03092001@cluster0.cwicyon.mongodb.net/?retryWrites=true&w=majority',
     secretKey: 'super-secret-key',
   };
   
